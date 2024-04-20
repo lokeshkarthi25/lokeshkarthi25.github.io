dd2e5e78b3acbd6bd13c081e83eeb58a2ae1fee6
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-04-20 12:52:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('contact-form');
+//     const form = document.getElementById('contact-form');
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
 
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
+//         const name = document.getElementById('name').value;
+//         const email = document.getElementById('email').value;
+//         const message = document.getElementById('message').value;
 
-        // Here you can add code to handle the form submission, such as sending the data to a server
-        console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+//         // Here you can add code to handle the form submission, such as sending the data to a server
+//         console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
         
-        alert('Message sent successfully!');
-    });
-});
+//         alert('Message sent successfully!');
+//     });
+// });
 
 const slides=document.querySelectorAll(".gallery img");
 let slideIndex=0;
